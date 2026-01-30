e0d973b53fe6aaed7597dc9aa7e7094adb5c65e9
ectasy663 <namansingh4680@gmail.com>
2026-01-30 19:48:22 +0530

    minore bug fixes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://namansinghpanwar.vercel.app';
+  const baseUrl = 'https://namansinghpanwar.in';
   const lastModified = new Date('2026-01-30');
 
   return [
