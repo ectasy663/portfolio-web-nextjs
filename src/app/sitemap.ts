a9edbb8cfd73b55633079bc97189a421cd325852
ectasy663 <namansingh4680@gmail.com>
2026-05-04 11:52:19 +0530

    Added sitemap file essentials

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,12 +2,11 @@ import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://namansinghpanwar.in';
-  const lastModified = new Date('2026-01-30');
 
   return [
     {
       url: baseUrl,
-      lastModified,
+      lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
     },
